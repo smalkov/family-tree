a3f19a99f875e0f7bf2cb89f20f2ac26124ef3b3
Малков Сергей Юрьевич <pewpewinmyhead@gmail.com>
2025-05-08 20:47:41 +0300

    fix node key

--- src/components/MainTree/MainTree.tsx
+++ src/components/MainTree/MainTree.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useEffect } from "react";
+import React, { useMemo, useState, useEffect } from "react";
 import { observer } from "mobx-react-lite";
 import { toJS } from "mobx";
 import ReactFamilyTree from "react-family-tree";
@@ -30,36 +30,37 @@ const MainTreeProto = ({ familyTreeState }: any) => {
   if (!tree?.length) {
     return <div style={{ textAlign: "center", marginTop: "100px" }}>Нет данных</div>;
   }
+  console.log("full tree", toJS(tree));
 
-  const { canvas } = calcTree(tree, { rootId: rootId });
-
-  console.log("tree", toJS(tree));
+  const { nodes, canvas } = calcTree(tree, { rootId: rootId });
 
   const contentW = canvas.width * (WIDTH / 2);
   const contentH = canvas.height * (HEIGHT / 2);
 
+  console.log("nodes", nodes);
+
   return (
     <Viewport className={styles.root}>
       <TransformWrapper minScale={0.2} maxScale={4} limitToBounds centerOnInit centerZoomedOut smooth>
-        {/* <TransformComponent contentClass={styles.wrapper} wrapperClass={styles.wrapper}> */}
         <TransformComponent
           wrapperStyle={{ width: "100%", height: "100%" }}
           contentStyle={{ width: contentW, height: contentH, position: "relative" }}
         >
           <ReactFamilyTree
-            nodes={toJS(tree)}
+            nodes={nodes}
             rootId={rootId}
             width={WIDTH}
             height={HEIGHT}
-            placeholders
             className={styles.tree}
             renderNode={(node) => {
               return (
                 <FamilyNode
-                  key={node.id}
+                  key={`${node.id}-${node.left}-${node.top}`}
                   isRoot={node.id === rootId}
                   node={node}
-                  onSubClick={(id) => setRootId(id)}
+                  onSubClick={(id) => {
+                    setRootId(id);
+                  }}
                   handleNodeClick={(node) => console.log("click", node)}
                   style={{
                     width: WIDTH,
